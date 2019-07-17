29283ddf377380dc029c9e57c6cdc483e3168483
ropeks <ropeksmilioner@gmail.com>
2019-07-17 15:57:17 +0200

    write findSteps and add helper methods

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -9,12 +9,15 @@ function findById(id) {
     return db('schemes').where({ id });
 }
   
-function findSteps() {
-  
+function findSteps(id) {
+    return db("steps")
+      .select('steps.id as id', 'scheme_name', 'step_number', 'instructions')
+      .join('schemes', 'steps.scheme_id', 'schemes.id')
+      .where('schemes.id', id);
 }
   
-function add() {
-  
+function add(scheme) {
+    return db('schemes').insert(scheme);
 }
   
 function update() {
