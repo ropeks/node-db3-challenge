9f13c39b192683a5b529bb00b46923a4ed3707e9
ropeks <ropeksmilioner@gmail.com>
2019-07-17 15:19:42 +0200

    prepare scheme-model.js for knex queries

---
+++ schemes/scheme-model.js
@@ -0,0 +1,35 @@
+const knex = require('knex');
+const db = knex(require('../knexfile').development);
+
+function find() {
+   
+}
+  
+function findById() {
+  
+}
+  
+function findSteps() {
+  
+}
+  
+function add() {
+  
+}
+  
+function update() {
+  
+}
+  
+function remove() {
+  
+}
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
